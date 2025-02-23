954590d006bb2d3713511ddfa886186b2183e780
mykola-ovchynnik <ovchynnik.mykola@gmail.com>
2025-02-23 12:36:55 +0100

    Refactor Pythagoras tree

--- task2.py
+++ task2.py
@@ -1,59 +1,40 @@
-import turtle
 import math
+import turtle
 
 
-def pythagoras_tree(t, branch_len, level):
-    """
-    Функція рекурсивно малює 'дерево Піфагора'.
-    t          : об'єкт Turtle
-    branch_len : довжина поточної гілки
-    level      : рівень рекурсії, що залишився
-    """
+def setup_turtle():
+    screen = turtle.Screen()
+    screen.setup(width=800, height=600)
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.left(90)
+    t.penup()
+    t.backward(200)
+    t.pendown()
+    t.speed(0)
+    return t
+
+
+def pythagoras_tree(t, level, branch_len: int | float = 100):
     if level == 0:
         return
 
-    # Намалювати "стовбур" (поточну гілку)
     t.forward(branch_len)
 
-    # Ліва гілка
     t.left(45)
-    pythagoras_tree(t, branch_len / math.sqrt(2), level - 1)
+    pythagoras_tree(t, level - 1, branch_len / math.sqrt(2))
 
-    # Перехід до правої гілки
     t.right(90)
-    pythagoras_tree(t, branch_len / math.sqrt(2), level - 1)
+    pythagoras_tree(t, level - 1, branch_len / math.sqrt(2))
 
-    # Повернутися в початковий нахил
     t.left(45)
-
-    # Повернутися "вниз" по стовбуру
     t.backward(branch_len)
 
 
 def main():
-    # Зчитуємо від користувача бажаний рівень рекурсії
-    level = int(input("Вкажіть рівень рекурсії: "))
-
-    # Ініціалізація вікна та 'черепашки'
-    screen = turtle.Screen()
-    screen.setup(width=800, height=600)
-    t = turtle.Turtle()
-
-    # Повертаємо черепашку стовбуром догори
-    t.left(90)
-
-    # Зсунемося трохи вниз, щоб дерево повністю влізло у вікно
-    t.penup()
-    t.backward(200)
-    t.pendown()
-
-    # Швидкість малювання (0 – найшвидша)
-    t.speed(0)
-
-    # Викликаємо рекурсивну функцію
-    pythagoras_tree(t, 100, level)
-
-    # Залишаємо вікно відкритим
+    level = int(input("Enter the recursion level: "))
+    t = setup_turtle()
+    pythagoras_tree(t, level, 100)
     turtle.done()
 
 
